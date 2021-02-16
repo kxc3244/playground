4b79dc4218bb9439f5f7a7c9ccc66a733cbf8f13
kxc3244 <kevin.chawla@mavs.uta.edu>
2021-02-15 21:58:11 -0600

    commit message

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Kevin Chawla
+        </p>
+        <p>
+          Kevin Chawla
+        </p>
+          Kevin Chawla
+        <p> 
+        </p>
+        <p>
+          Kevin Chawla
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
