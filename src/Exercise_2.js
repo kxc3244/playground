9b308bb4f17107846d18409443af4f1b69f53493
kxc3244 <kevin.chawla@mavs.uta.edu>
2021-02-18 17:19:37 -0600

    Added responsive exercise with css units

--- src/Exercise_2.js
+++ src/Exercise_2.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './exercise_2.css';
 
 class Exercise_2 extends React.Component {
     render() {
        return (
           <div>
-            <h1>Put stuff here</h1>
+            <h1 className="header">Header</h1> 
+            <h1 className="Hero">Hero</h1> 
+            <h1 className="Content">Content</h1>
+            <h1 className="sidebar">Sidebar</h1>
+            <h1 className="footer">Footer</h1>
+
+            
           </div>
+
+    
        )
     }
   }
